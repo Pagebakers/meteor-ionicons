93f55f6f1a28dfb43437efc996c9a212c4d96707
Eelco Wiersma <eelco@pagebakers.nl>
2015-06-17 18:35:50 +0700

    Changed the way css is included

---
+++ ionicons.js
@@ -0,0 +1,7 @@
+var head = document.getElementsByTagName('head')[0];
+var s = document.createElement('link');
+
+s.type = 'text/css';
+s.rel = 'stylesheet';
+s.href = '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css';
+head.appendChild(style);
--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
 	summary: "Latest version of Ionicons",
-	version: "2.0.1",
+	version: "2.0.1-1",
 	name: "pagebakers:ionicons",
 	git: "https://github.com/Pagebakers/meteor-ionicons.git"
 });
 
 Package.on_use(function (api){
 	api.add_files([
-    'ionicons.html'
+    'ionicons.js'
   ], 'client');
 });
