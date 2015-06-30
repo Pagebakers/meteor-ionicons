44e640684d469112c3bb37b0986cdb0b2f0ef556
Eelco Wiersma <eelco@pagebakers.nl>
2015-06-30 09:11:10 +0200

    Fixed version numbering

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "Latest version of Ionicons",
-	version: "2.0.1-1",
+	version: "2.0.1_1",
 	name: "pagebakers:ionicons",
 	git: "https://github.com/Pagebakers/meteor-ionicons.git"
 });
